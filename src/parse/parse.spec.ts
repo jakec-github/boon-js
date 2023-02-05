55e4525533ff2d37d4de7fb70f1128e7ff351484
Jake Chorley <jakechorley@192.168.1.206>
2023-02-05 19:23:12 +0000

    Removes jest-in-case from parse tests

--- src/parse/parse.spec.ts
+++ src/parse/parse.spec.ts
@@ -1,243 +1,56 @@
-import cases from 'jest-in-case';
-
-import {
-  FIRST,
-  SECOND,
-  THIRD,
-  FOURTH,
-  FIFTH,
-  AND,
-  NOT,
-  XOR,
-  OR,
-} from '../testConst';
 import { PostfixExpression } from '../types';
 
 import { parse } from './parse';
+import {
+  basic,
+  comments,
+  complex,
+  operatorPrecedence,
+  parentheses,
+  unhappyPath,
+} from './testData';
 
-interface EqualityTest {
-  expression: string;
-  postfix: PostfixExpression;
-}
-
-const checkEquality = ({ expression, postfix }: EqualityTest): void => {
+const checkEquality = (
+  expression: string,
+  postfix: PostfixExpression,
+): void => {
   expect(parse(expression)).toEqual(postfix);
 };
 
-interface ErrorTest {
-  expression: string;
-  message: string;
-}
-
-const checkError = ({ expression, message }: ErrorTest): void => {
+const checkError = (expression: string, message: string): void => {
   expect(() => {
     parse(expression);
   }).toThrow(message);
 };
 
 describe('parse', () => {
-  const basicTests = {
-    'parse a lone identifier': {
-      expression: 'first',
-      postfix: [FIRST],
-    },
-    'parse a simple expression': {
-      expression: 'first AND second',
-      postfix: [FIRST, SECOND, AND],
-    },
-    'handle the NOT operator': {
-      expression: 'NOT first',
-      postfix: [FIRST, NOT],
-    },
-  };
+  describe('Basic expressions', () => {
+    test.each(basic)('parse %s', (_, ...args) => checkEquality(...args));
+  });
 
-  cases('basic tests', checkEquality, basicTests);
+  describe('Operator precedence', () => {
+    test.each(operatorPrecedence)('parse expression: %s', (_, ...args) =>
+      checkEquality(...args),
+    );
+  });
 
-  const operatorPrecedence = {
-    'parse an expression with an AND then an OR operator': {
-      expression: 'first AND second OR third',
-      postfix: [FIRST, SECOND, AND, THIRD, OR],
-    },
-    'parse an expression with an AND then an AND operator': {
-      expression: 'first AND second AND third',
-      postfix: [FIRST, SECOND, AND, THIRD, AND],
-    },
-    'parse an expression with an XOR, AND then an OR operator': {
-      expression: 'first XOR second AND third OR fourth',
-      postfix: [FIRST, SECOND, XOR, THIRD, AND, FOURTH, OR],
-    },
-    'parse an expression with an OR then an AND operator': {
-      expression: 'first OR second AND third',
-      postfix: [FIRST, SECOND, THIRD, AND, OR],
-    },
-    'parse an expression with an AND then an XOR then an OR operator': {
-      expression: 'first AND second XOR third OR fourth',
-      postfix: [FIRST, SECOND, THIRD, XOR, AND, FOURTH, OR],
-    },
-    'parse an expression with an OR then an AND then an XOR operator': {
-      expression: 'first OR second AND third XOR fourth',
-      postfix: [FIRST, SECOND, THIRD, FOURTH, XOR, AND, OR],
-    },
-    'parse an expression with an OR then an XOR then an AND': {
-      expression: 'first OR second XOR third AND fourth',
-      postfix: [FIRST, SECOND, THIRD, XOR, FOURTH, AND, OR],
-    },
-    'parse an expression with an OR then an XOR then an AND with some NOTs': {
-      expression: 'first OR NOT second XOR third AND NOT fourth',
-      postfix: [FIRST, SECOND, NOT, THIRD, XOR, FOURTH, NOT, AND, OR],
-    },
-  };
+  describe('Parentheses', () => {
+    test.each(parentheses)('parse parentheses: %s', (_, ...args) =>
+      checkEquality(...args),
+    );
+  });
 
-  cases('operator precedence', checkEquality, operatorPrecedence);
+  describe('Comments', () => {
+    test.each(comments)('%s', (_, ...args) => checkEquality(...args));
+  });
 
-  const parentheses = {
-    'handle parentheses around an operator with lower precedence': {
-      expression: '(first OR second) AND third',
-      postfix: [FIRST, SECOND, OR, THIRD, AND],
-    },
-    'handle parentheses at the end of an expression': {
-      expression: 'first XOR (second AND third)',
-      postfix: [FIRST, SECOND, THIRD, AND, XOR],
-    },
-    'handle parentheses in the middle of an expression': {
-      expression: 'first XOR (second AND third) OR fourth',
-      postfix: [FIRST, SECOND, THIRD, AND, XOR, FOURTH, OR],
-    },
-    'handle nested parentheses': {
-      expression:
-        'NOT ((first OR second) AND NOT third) XOR (NOT fourth AND fifth)',
-      postfix: [
-        FIRST,
-        SECOND,
-        OR,
-        THIRD,
-        NOT,
-        AND,
-        NOT,
-        FOURTH,
-        NOT,
-        FIFTH,
-        AND,
-        XOR,
-      ],
-    },
-  };
+  describe('Complex expressions', () => {
+    test.each(complex)('parse complex expression: %s', (_, ...args) =>
+      checkEquality(...args),
+    );
+  });
 
-  cases('parentheses', checkEquality, parentheses);
-
-  const comments = {
-    'Removes trailing comment': {
-      expression: 'NOT first # Comment',
-      postfix: [FIRST, NOT],
-    },
-    'Removes leading comment': {
-      expression: `# Comment
-      NOT first`,
-      postfix: [FIRST, NOT],
-    },
-    'Removes subsequent comments': {
-      expression: `# Comment 1
-      # Comment 2
-      NOT first
-      # Comment 3
-      # Comment 4`,
-      postfix: [FIRST, NOT],
-    },
-    'Removes comments whereever they appear in the expression': {
-      expression: `# Comment
-      NOT # Comment
-      ( # Comment
-        first # Comment
-        AND # Comment
-        second # Comment
-      ) # Comment`,
-      postfix: [FIRST, SECOND, AND, NOT],
-    },
-  };
-
-  cases('comments', checkEquality, comments);
-
-  const complexTests = {
-    'handle a complicated expression with multiple NOT operators': {
-      expression: 'NOT first AND second AND NOT third XOR NOT fourth',
-      postfix: [FIRST, NOT, SECOND, AND, THIRD, NOT, FOURTH, NOT, XOR, AND],
-    },
-    'handle a complicated expression with multiple NOT operators and parentheses': {
-      expression:
-        'NOT (first AND second AND NOT third) XOR (NOT fourth XOR fifth)',
-      postfix: [
-        FIRST,
-        SECOND,
-        AND,
-        THIRD,
-        NOT,
-        AND,
-        NOT,
-        FOURTH,
-        NOT,
-        FIFTH,
-        XOR,
-        XOR,
-      ],
-    },
-  };
-
-  cases('complex tests', checkEquality, complexTests);
-
-  const unhappyTests = {
-    'throw if terms are out of order': {
-      expression: 'AND first second',
-      message: 'Invalid token',
-    },
-    'throw if the second term is missing': {
-      expression: 'first AND',
-      message: 'Unexpected end of expression',
-    },
-    'throw if the operator is repeated': {
-      expression: 'first AND AND second',
-      message: 'Invalid token',
-    },
-    'throw from an expression with an OR then a hanging AND operator': {
-      expression: 'first OR second AND ',
-      message: 'Unexpected end of expression',
-    },
-    'throw on two NOT operators in a row': {
-      expression: 'NOT NOT',
-      message: 'Invalid token',
-    },
-    'throw on lone NOT operator': {
-      expression: 'NOT',
-      message: 'Unexpected end of expression',
-    },
-    'throw on lone nested NOT operator': {
-      expression: '(NOT)',
-      messsage: 'Inavlid token',
-    },
-    'throw if there are too many closing parentheses': {
-      expression: 'NOT ((first OR second) AND NOT third))',
-      message: 'Invalid token',
-    },
-    'throw if there are too many opening parentheses': {
-      expression: 'NOT (((first OR second) AND NOT third)',
-      message: 'Unexpected end of expression',
-    },
-    'throw if an open parenthesis follows an identifier': {
-      expression: 'first (',
-      message: 'Invalid token',
-    },
-    'throw on empty expression': {
-      expression: '',
-      message: 'Unexpected end of expression',
-    },
-    'throw on expression with only a comment': {
-      expression: '# Comment',
-      message: 'Unexpected end of expression',
-    },
-    'throw if argument is not a string': {
-      expression: null as string,
-      message: 'Expected string but received object',
-    },
-  };
-
-  cases('Unhappy path', checkError, unhappyTests);
+  describe('Unhappy path', () => {
+    test.each(unhappyPath)('throw on %s', (_, ...args) => checkError(...args));
+  });
 });
---
+++ src/parse/testData.ts
@@ -0,0 +1,163 @@
+import {
+  FIRST,
+  SECOND,
+  THIRD,
+  FOURTH,
+  FIFTH,
+  AND,
+  NOT,
+  XOR,
+  OR,
+} from '../testConst';
+import { PostfixExpression } from '../types';
+
+export const basic: [string, string, PostfixExpression][] = [
+  ['lone identifier', 'first', [FIRST]],
+  ['simple expression', 'first AND second', [FIRST, SECOND, AND]],
+  ['NOT operator', 'NOT first', [FIRST, NOT]],
+];
+
+export const operatorPrecedence: [string, string, PostfixExpression][] = [
+  ['AND then OR', 'first AND second OR third', [FIRST, SECOND, AND, THIRD, OR]],
+  [
+    'AND then AND',
+    'first AND second AND third',
+    [FIRST, SECOND, AND, THIRD, AND],
+  ],
+  ['OR then OR', 'first OR second OR third', [FIRST, SECOND, OR, THIRD, OR]],
+  ['OR then AND', 'first OR second AND third', [FIRST, SECOND, THIRD, AND, OR]],
+  [
+    'XOR, AND then OR',
+    'first XOR second AND third OR fourth',
+    [FIRST, SECOND, XOR, THIRD, AND, FOURTH, OR],
+  ],
+  [
+    'AND, XOR then OR',
+    'first AND second XOR third OR fourth',
+    [FIRST, SECOND, THIRD, XOR, AND, FOURTH, OR],
+  ],
+  ['XOR then OR', 'first XOR second OR third', [FIRST, SECOND, XOR, THIRD, OR]],
+  [
+    'XOR then AND',
+    'first XOR second AND third',
+    [FIRST, SECOND, XOR, THIRD, AND],
+  ],
+  [
+    'OR, AND then XOR',
+    'first OR second AND third XOR fourth',
+    [FIRST, SECOND, THIRD, FOURTH, XOR, AND, OR],
+  ],
+  [
+    'OR, XOR then AND',
+    'first OR second XOR third AND fourth',
+    [FIRST, SECOND, THIRD, XOR, FOURTH, AND, OR],
+  ],
+  [
+    'OR, XOR, AND with some NOT operators',
+    'first OR NOT second XOR third AND NOT fourth',
+    [FIRST, SECOND, NOT, THIRD, XOR, FOURTH, NOT, AND, OR],
+  ],
+];
+
+export const parentheses: [string, string, PostfixExpression][] = [
+  ['around a single identifier', '(first)', [FIRST]],
+  ['around a NOT operator', '(NOT first)', [FIRST, NOT]],
+  [
+    'around an operator with lower precedence',
+    '(first AND second) OR third',
+    [FIRST, SECOND, AND, THIRD, OR],
+  ],
+  [
+    'at the end of an expression',
+    'first AND (second OR third)',
+    [FIRST, SECOND, THIRD, OR, AND], // Curious to see if this is right
+  ],
+  [
+    'in the middle of an expression',
+    'first XOR (second AND third) OR fourth',
+    [FIRST, SECOND, THIRD, AND, XOR, FOURTH, OR],
+  ],
+  ['nested', '((first))', [FIRST]],
+  [
+    'complex nested',
+    '((first AND second) OR third)',
+    [FIRST, SECOND, AND, THIRD, OR],
+  ],
+  [
+    'long complex nested',
+    'NOT ((first OR second) AND NOT third) XOR (NOT fourth AND fifth)',
+    [FIRST, SECOND, OR, THIRD, NOT, AND, NOT, FOURTH, NOT, FIFTH, AND, XOR],
+  ],
+];
+
+export const comments: [string, string, PostfixExpression][] = [
+  ['Removes trailing comment', 'NOT first # comment', [FIRST, NOT]],
+  ['Removes leading comment', '# comment\nNOT first', [FIRST, NOT]],
+  [
+    'Removes subsequent comments',
+    '# comment 1\n# comment 2\nNOT first # comment 3\n# comment 4',
+    [FIRST, NOT],
+  ],
+  [
+    'Removes comments wherever they appear in the expression',
+    `# Comment
+      NOT # Comment
+      ( # Comment
+        first # Comment
+        AND # Comment
+        second # Comment
+      ) # Comment`,
+    [FIRST, SECOND, AND, NOT],
+  ],
+];
+
+export const complex: [string, string, PostfixExpression][] = [
+  [
+    'multiple NOT operators',
+    'NOT first AND second AND NOT third XOR NOT fourth',
+    [FIRST, NOT, SECOND, AND, THIRD, NOT, FOURTH, NOT, XOR, AND],
+  ],
+  [
+    'multiple NOT operators and parentheses',
+    'NOT (first AND second AND NOT third) XOR (NOT fourth XOR fifth)',
+    [FIRST, SECOND, AND, THIRD, NOT, AND, NOT, FOURTH, NOT, FIFTH, XOR, XOR],
+  ],
+];
+
+export const unhappyPath: [string, string, string][] = [
+  ['out of order identifiers', 'AND first second', 'Invalid token'],
+  ['missing second identifier', 'first AND', 'Unexpected end of expression'],
+  ['missing first identifier', 'AND second', 'Invalid token'],
+  ['repeated operator', 'first AND AND second', 'Invalid token'],
+  ['hanging AND', 'first OR second AND', 'Unexpected end of expression'],
+  ['two NOT operators in a row', 'NOT NOT first', 'Invalid token'],
+  ['lone NOT operator', 'NOT', 'Unexpected end of expression'],
+  [
+    'lone nested NOT operator',
+    '(NOT)',
+    'Unexpected character: ). Operators should be separated using whitespace',
+  ],
+  [
+    'too many closing parentheses',
+    'NOT ((first OR second) AND NOT third))',
+    'Invalid token',
+  ],
+  [
+    'too many opening parentheses',
+    'NOT (((first OR second) AND NOT third)',
+    'Unexpected end of expression',
+  ],
+  ['open parenthesus follows an identifier', 'first (', 'Invalid token'],
+  ['empty expression', '', 'Unexpected end of expression'],
+  ['expression with onlu whitespace', ' ', 'Unexpected end of expression'],
+  [
+    'expression with only a comment',
+    '# comment',
+    'Unexpected end of expression',
+  ],
+  [
+    'non-string argument',
+    null as string,
+    'Expected string but received object',
+  ],
+];
